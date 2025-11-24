b2f8227cef7b3702c776b76ddf78ef2a209ca331
kviatkivskyinet-bit <kviatkivskyi.net@gmail.com>
2025-11-24 15:33:27 +0200

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  //output: 'export',
+  //distDir: 'out',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true
+  },
+  trailingSlash: true,
+  // Disable server-side features for static export
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  }
+};
+
+module.exports = nextConfig;
